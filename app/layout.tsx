37e793d375d02ac1d4b4b0bec02cf852a68a7eab
tsaqib <tsaqib@softwareseni.com>
2025-09-09 11:08:14 +0700

    refactor: remove unused imports and clean up seed route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter,lusitana } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
